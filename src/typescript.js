bac353e617866f9654731c673db6c6e5288ae3d7
javierscode <javihp79@gmail.com>
2022-09-12 14:12:14 +0200

    typescript eslint config

---
+++ src/typescript.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    './base.js',
+  ],
+  plugins: ['@typescript-eslint'],
+}
