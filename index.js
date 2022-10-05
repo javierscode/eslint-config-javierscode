891c88581ee0c70f3500607f5507b61e070e17a6
javierscode <javihp79@gmail.com>
2022-10-05 12:01:58 +0200

    fix: another method to export configs

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const eslintrc = require('./src/base.js')
-const typescript = require('./src/typescript.js')
-const react = require('./src/react-typescript.js')
-const reactTypescript = require('./src/react-typescript.js')
+const eslintrc = require('./.eslintrc.js')
 
 module.exports = eslintrc
-exports.typescript = typescript
-exports.react = react
-exports['react-typescript'] = reactTypescript
